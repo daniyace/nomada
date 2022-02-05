9f0d4ca899d72470f7782a5c2df4eae197c7bdb6
Angel Ayala <ayalaceja@hotmail.com>
2022-02-04 19:20:22 -0600

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,38 @@
 module.exports = {
   siteMetadata: {
-      title: `nomada`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `nomada`,
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-sass"]
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-sass',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Nómada`,
+        short_name: `Nómada`,
+        start_url: `/`,
+        description: `Carta de nómada`,
+        icon: `src/images/icon.png`,
+        icon_options: {
+          purpose: `any maskable`,
+        },
+        lang: `es`,
+        background_color: `#321f61`,
+        theme_color: `#452b86`,
+        display: `standalone`,
+        crossOrigin: `use-credentials`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-offline`,
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [`Montserrat\:300,400,500,300i,700`],
+        display: 'swap',
+      },
+    },
+  ],
 };
