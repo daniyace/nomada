e60272544cdc7ee673890a80f0485ff4a8cc5b30
Angel Ayala <ayalaceja@hotmail.com>
2022-02-05 00:33:19 -0600

    push

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `nomada`,
-    siteUrl: `https://daniyace.github.io/nomada/`,
+    siteUrl: `https://daniyace.github.io/`,
   },
   pathPrefix: '/nomada',
   plugins: [
