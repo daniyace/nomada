1eae7a9d042cfcf7caad0ee85ec68774700b3519
Angel Ayala <ayalaceja@hotmail.com>
2022-02-05 00:09:33 -0600

    Updates

---
+++ component---src-pages-404-jsx-c8841b242375ac91951f.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunknomada=self.webpackChunknomada||[]).push([[256],{7291:function(n,e,u){u.r(e);var a=u(7294);e.default=function(){return a.createElement("div",null,"NotFound")}}}]);
+//# sourceMappingURL=component---src-pages-404-jsx-c8841b242375ac91951f.js.map
