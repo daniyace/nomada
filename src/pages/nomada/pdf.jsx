fd0aa86915cd1bd68f99378f86e73a4edc8e82af
Angel Ayala <ayalaceja@hotmail.com>
2022-02-26 15:40:32 -0600

    redirect

--- src/pages/nomada/pdf.jsx
+++ src/pages/nomada/pdf.jsx
@@ -1,19 +1,12 @@
-import React from 'react';
-import Seo from '../../components/Seo';
-import PDF from '../../docs/NOMADA.pdf';
-import M1 from '../../docs/m1.png';
-import M2 from '../../docs/m2.png';
+import React, { useEffect } from 'react';
+import { navigate } from 'gatsby';
 
-const pdf = () => {
-  return (
-    <>
-      <Seo title='Nómada' />
-      <div className='container'>
-        <img src={M1} alt='' className='img-fluid' />
-        <img src={M2} alt='' className='img-fluid' />
-      </div>
-    </>
-  );
+const Pdf = () => {
+  useEffect(() => {
+    navigate('/nomada/');
+  }, []);
+
+  return <></>;
 };
 
-export default pdf;
+export default Pdf;
